7b42c60b2dc1126179affb2bbb4416c17864bdab
ayleenhn <132100078+ayleenhn@users.noreply.github.com>
2023-11-17 16:06:30 -0600

    Add files via upload
    Second Commit

--- Lab6P2_AyleenEscobar/src/Main.java
+++ Lab6P2_AyleenEscobar/src/Main.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Date;
+import javax.swing.JOptionPane;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
@@ -17,6 +19,8 @@ public class Main extends javax.swing.JFrame {
      */
     public Main() {
         initComponents();
+        jL_Iniciar.setVisible(true);
+        jP_CrearCuenta.setVisible(false);
     }
 
     /**
@@ -38,12 +42,21 @@ public class Main extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jDateChooser1 = new com.toedter.calendar.JDateChooser();
+        jTF_nombre = new javax.swing.JTextField();
+        jTF_correo = new javax.swing.JTextField();
+        jTF_pais = new javax.swing.JTextField();
+        jd_fecha = new com.toedter.calendar.JDateChooser();
         jButton1 = new javax.swing.JButton();
+        jL_edad = new javax.swing.JLabel();
         jLabel6 = new javax.swing.JLabel();
+        jD_Menu = new javax.swing.JDialog();
+        jPanel1 = new javax.swing.JPanel();
+        jP_IniciarSesion = new javax.swing.JPanel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jTF_nombre1 = new javax.swing.JTextField();
+        jLabel9 = new javax.swing.JLabel();
+        jPasswordField1 = new javax.swing.JPasswordField();
 
         jP_Inicio.setBackground(new java.awt.Color(0, 0, 0));
         jP_Inicio.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -51,6 +64,11 @@ public class Main extends javax.swing.JFrame {
         jL_crear.setFont(new java.awt.Font("Five Nights at Freddy's", 1, 40)); // NOI18N
         jL_crear.setForeground(new java.awt.Color(255, 255, 255));
         jL_crear.setText("Crear una cuenta");
+        jL_crear.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jL_crearMouseClicked(evt);
+            }
+        });
         jP_Inicio.add(jL_crear, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 300, 250, -1));
 
         jL_bglogin.setFont(new java.awt.Font("Five Nights at Freddy's", 1, 40)); // NOI18N
@@ -66,11 +84,6 @@ public class Main extends javax.swing.JFrame {
         jL_Iniciar.setFont(new java.awt.Font("Five Nights at Freddy's", 1, 40)); // NOI18N
         jL_Iniciar.setForeground(new java.awt.Color(255, 255, 255));
         jL_Iniciar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/650294(1).jpg"))); // NOI18N
-        jL_Iniciar.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jL_IniciarMouseClicked(evt);
-            }
-        });
         jP_Inicio.add(jL_Iniciar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 760, -1));
 
         jP_CrearCuenta.setBackground(new java.awt.Color(0, 0, 0));
@@ -102,24 +115,24 @@ public class Main extends javax.swing.JFrame {
         jLabel5.setText("Fecha de nacimiento:");
         jP_CrearCuenta.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 290, -1, -1));
 
-        jTextField1.setForeground(new java.awt.Color(0, 0, 0));
-        jP_CrearCuenta.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 170, 206, -1));
+        jTF_nombre.setForeground(new java.awt.Color(0, 0, 0));
+        jP_CrearCuenta.add(jTF_nombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 170, 206, -1));
 
-        jTextField2.setForeground(new java.awt.Color(0, 0, 0));
-        jP_CrearCuenta.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 210, 206, -1));
-
-        jTextField3.setForeground(new java.awt.Color(0, 0, 0));
-        jP_CrearCuenta.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 250, 206, -1));
-
-        jDateChooser1.setForeground(new java.awt.Color(255, 255, 255));
-        jP_CrearCuenta.add(jDateChooser1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 290, 206, -1));
-
-        jButton1.setText("Crear Cuenta");
-        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButton1MouseClicked(evt);
+        jTF_correo.setForeground(new java.awt.Color(0, 0, 0));
+        jTF_correo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTF_correoActionPerformed(evt);
             }
         });
+        jP_CrearCuenta.add(jTF_correo, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 210, 206, -1));
+
+        jTF_pais.setForeground(new java.awt.Color(0, 0, 0));
+        jP_CrearCuenta.add(jTF_pais, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 250, 206, -1));
+
+        jd_fecha.setForeground(new java.awt.Color(255, 255, 255));
+        jP_CrearCuenta.add(jd_fecha, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 290, 206, -1));
+
+        jButton1.setText("Crear Cuenta");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
@@ -127,45 +140,137 @@ public class Main extends javax.swing.JFrame {
         });
         jP_CrearCuenta.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 350, -1, -1));
 
+        jL_edad.setFont(new java.awt.Font("Segoe UI", 3, 12)); // NOI18N
+        jL_edad.setForeground(new java.awt.Color(255, 153, 153));
+        jL_edad.setText("La edad debe ser mayor de 12.");
+        jP_CrearCuenta.add(jL_edad, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, 320, -1, -1));
+
         jLabel6.setFont(new java.awt.Font("Roboto", 0, 14)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/652823(1).png"))); // NOI18N
         jP_CrearCuenta.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 760, -1));
 
+        jPanel1.setBackground(new java.awt.Color(102, 255, 102));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 760, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 470, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout jD_MenuLayout = new javax.swing.GroupLayout(jD_Menu.getContentPane());
+        jD_Menu.getContentPane().setLayout(jD_MenuLayout);
+        jD_MenuLayout.setHorizontalGroup(
+            jD_MenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        jD_MenuLayout.setVerticalGroup(
+            jD_MenuLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                formKeyPressed(evt);
+            }
+        });
+
+        jP_IniciarSesion.setBackground(new java.awt.Color(0, 0, 0));
+        jP_IniciarSesion.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jLabel7.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel7.setText("Usuario: ");
+        jP_IniciarSesion.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 180, -1, -1));
+
+        jLabel8.setFont(new java.awt.Font("Roboto", 1, 14)); // NOI18N
+        jLabel8.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel8.setText("Contraseña: ");
+        jP_IniciarSesion.add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 210, -1, -1));
+
+        jTF_nombre1.setForeground(new java.awt.Color(0, 0, 0));
+        jP_IniciarSesion.add(jTF_nombre1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 170, 206, -1));
+
+        jLabel9.setFont(new java.awt.Font("Roboto Medium", 0, 36)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText("Iniciar Sesión");
+        jP_IniciarSesion.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 100, -1, -1));
+
+        jPasswordField1.setText("jPasswordField1");
+        jP_IniciarSesion.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 210, 200, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 760, Short.MAX_VALUE)
+            .addComponent(jP_IniciarSesion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 470, Short.MAX_VALUE)
+            .addComponent(jP_IniciarSesion, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jL_IniciarMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jL_IniciarMouseClicked
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_jL_IniciarMouseClicked
-
     private void jL_bgloginMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jL_bgloginMouseClicked
         // TODO add your handling code here:
+        jP_IniciarSesion.setVisible(true);
+        jP_CrearCuenta.setVisible(false);
+        jL_Iniciar.setVisible(false);
     }//GEN-LAST:event_jL_bgloginMouseClicked
-
-    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_jButton1MouseClicked
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
         
+        if (jTF_nombre.getText()==""|| jTF_correo.getText()=="" || jTF_pais.getText()=="" || jd_fecha.getDate()==null) {
+            JOptionPane.showMessageDialog(this, "Llene las casillas.");
+            
+        }else if (!edadvalida(jd_fecha.getDate())) {
+            jL_edad.setVisible(true);
+            JOptionPane.showMessageDialog(this, "Usuario no creado existosamente");
+        } else {
+             usuarios.add(new Usuario(jTF_nombre.getText(),
+                                    jTF_correo.getText(),
+                                    jTF_pais.getText(),
+                                    jd_fecha.getDate()
+            )
+            );
+            jTF_nombre.setText("");
+            jTF_correo.setText("");
+            jTF_pais.setText("");
+            jd_fecha.setDate(new Date());
+            jL_edad.setVisible(false);
+            JOptionPane.showMessageDialog(this, "Usuario creado existosamente");
+
+        }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTF_correoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTF_correoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTF_correoActionPerformed
+
+    private void jL_crearMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jL_crearMouseClicked
+        // TODO add your handling code here:
+        jL_Iniciar.setVisible(false);
+        jP_CrearCuenta.setVisible(true);
+        jL_edad.setVisible(false);
+    }//GEN-LAST:event_jL_crearMouseClicked
+
+    private void formKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_formKeyPressed
+        // TODO add your handling code here:
+        if (evt.getKeyCode()==27) {
+            jD_Menu.setVisible(true);
+            jD_Menu.pack();
+            jD_Menu.setLocationRelativeTo(this);
+        }
+    }//GEN-LAST:event_formKeyPressed
 
     /**
      * @param args the command line arguments
@@ -201,23 +306,40 @@ public class Main extends javax.swing.JFrame {
             }
         });
     }
+    
+    public static boolean edadvalida(Date fecha) {
+        Date fechaActual = new Date();
+        long edad = fechaActual.getTime() - fecha.getTime();
+        long edadActual = edad / (1000L * 60 * 60 * 24 * 365);
+        return edadActual >= 12;
+    }
+    
     private ArrayList<Usuario> usuarios= new ArrayList();
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private com.toedter.calendar.JDateChooser jDateChooser1;
+    private javax.swing.JDialog jD_Menu;
     private javax.swing.JLabel jL_Iniciar;
     private javax.swing.JLabel jL_bglogin;
     private javax.swing.JLabel jL_crear;
+    private javax.swing.JLabel jL_edad;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jP_CrearCuenta;
+    private javax.swing.JPanel jP_IniciarSesion;
     private javax.swing.JPanel jP_Inicio;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPasswordField jPasswordField1;
+    private javax.swing.JTextField jTF_correo;
+    private javax.swing.JTextField jTF_nombre;
+    private javax.swing.JTextField jTF_nombre1;
+    private javax.swing.JTextField jTF_pais;
+    private com.toedter.calendar.JDateChooser jd_fecha;
     // End of variables declaration//GEN-END:variables
 }
